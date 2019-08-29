329365f34f35b92ce5a795065ec1b6d34e39e28f
jo <>
2019-08-29 16:36:12 +0900

    딥링크 액티비티 추가

---
+++ src/android/DeepLinkActivity.java
@@ -0,0 +1,58 @@
+package kr.co.wisetracker.tracker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+
+import kr.co.wisetracker.tracker.WiseTracker;
+
+public class DeepLinkActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setSDK(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setSDK(intent);
+    }
+
+    private void setSDK(Intent intent) {
+        try {
+            Log.i("[wisetracker]", "set sdk by deep link!!");
+            if (intent == null || intent.getData() == null) {
+                Log.i("[wisetracker]", "intent is null !!");
+                return;
+            }
+            if (!WiseTracker.checkWisetrackerSDKInitState()) {
+                WiseTracker.initWithIntent(this, intent);
+            } else {
+                WiseTracker.checkReferrer(intent);
+            }
+            if (getIntent() != null && getIntent().getData() != null && !TextUtils.isEmpty(getIntent().getData().toString())) {
+                String deepLink = getIntent().getData().toString();
+                Log.i("[wisetracker]", "deeplink : " + deepLink);
+            }
+            moveToMainActivity();
+        } catch (Exception e) {
+            Log.e("[wisetracker]", "set sdk error !!", e);
+        }
+    }
+
+    private void moveToMainActivity() {
+        try {
+            Intent intent = getPackageManager().getLaunchIntentForPackage(getPackageName());
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        } catch (Exception e) {
+            Log.i("[wisetracker]", "move to main activity error !!", e);
+        }
+    }
+
+}
